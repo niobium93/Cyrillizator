c1d5307ed09dd544732691b7c38de8f160e1114c
Andrius Lukoševičius <niobium93@gmail.com>
2015-05-31 01:57:22 +0300

    Made indentation look better.

--- Cyrillizator.user.js
+++ Cyrillizator.user.js
@@ -7,60 +7,64 @@
 // @grant        none
 // ==/UserScript==
 
-   var arReplacements = {
-		'B': 'Б',
-		'V': 'В',
-		'G': 'Г',
-		'D': 'Д',
-		'Ž': 'Ж',
-		'Z': 'З',
-		'I': 'И',
-		'L': 'Л',
-		'N': 'Н',
-		'P': 'П',
-		'R': 'Р',
-		'S': 'С',
-		'U': 'У',
-		'F': 'Ф',
-        'H': 'Х',
-		'C': 'Ц',
-        'Č': 'Ч',
-        'Š': 'Ш',
-        'E': 'Э',
-        'b': 'б',
-		'v': 'в',
-		'g': 'г',
-		'd': 'д',
-		'ž': 'ж',
-		'z': 'з',
-		'i': 'и',
-		'l': 'л',
-		'n': 'н',
-		'p': 'п',
-		'r': 'р',
-		's': 'с',
-		'u': 'у',
-		'f': 'ф',
-        'h': 'х',
-		'c': 'ц',
-        'č': 'ч',
-        'š': 'ш',
-        'e': 'э'};
+var arReplacements = {
+	'B': 'Б',
+	'V': 'В',
+	'G': 'Г',
+	'D': 'Д',
+	'Ž': 'Ж',
+	'Z': 'З',
+	'I': 'И',
+	'L': 'Л',
+	'N': 'Н',
+	'P': 'П',
+	'R': 'Р',
+	'S': 'С',
+	'U': 'У',
+	'F': 'Ф',
+	'H': 'Х',
+	'C': 'Ц',
+	'Č': 'Ч',
+	'Š': 'Ш',
+	'E': 'Э',
+	'b': 'б',
+	'v': 'в',
+	'g': 'г',
+	'd': 'д',
+	'ž': 'ж',
+	'z': 'з',
+	'i': 'и',
+	'l': 'л',
+	'n': 'н',
+	'p': 'п',
+	'r': 'р',
+	's': 'с',
+	'u': 'у',
+	'f': 'ф',
+	'h': 'х',
+	'c': 'ц',
+	'č': 'ч',
+	'š': 'ш',
+	'e': 'э'
+					};
 
-   var arRegex = new Array();
-   for (var sKey in arReplacements) {
-	   arRegex[sKey] = new RegExp(sKey, 'g');
-   }
+var arRegex = new Array();
+for (var sKey in arReplacements)
+{
+	arRegex[sKey] = new RegExp(sKey, 'g');
+}
 
-   var snapTextNodes = document.evaluate("//text()[" +
-	   "not(ancestor::script) and not(ancestor::style)]",
-	   document, null, XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE, null);
+var snapTextNodes = document.evaluate("//text()[" +
+					"not(ancestor::script) and not(ancestor::style)]",
+					document, null, XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE, null);
 
-   for (var i = snapTextNodes.snapshotLength - 1; i >= 0; i--) {
-	  var elmTextNode = snapTextNodes.snapshotItem(i);
-	  var sText = elmTextNode.data;
-	  for (var sKey in arReplacements) {
-		  sText = sText.replace(arRegex[sKey], arReplacements[sKey]);
-	  }
-	  elmTextNode.data = sText;
-  }
+for (var i = snapTextNodes.snapshotLength - 1; i >= 0; i--)
+{
+	var elmTextNode = snapTextNodes.snapshotItem(i);
+	var sText = elmTextNode.data;
+	for (var sKey in arReplacements)
+	{
+		sText = sText.replace(arRegex[sKey], arReplacements[sKey]);
+	}
+	elmTextNode.data = sText;
+}
